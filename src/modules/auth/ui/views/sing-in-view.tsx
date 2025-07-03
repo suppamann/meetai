5a539f83a1b9350a6d530b46099564b99092a6bb
suppamann <er.yellowflash@gmail.com>
2025-07-03 10:18:49 +0530

    Added form

--- src/modules/auth/ui/views/sing-in-view.tsx
+++ src/modules/auth/ui/views/sing-in-view.tsx
@@ -18,28 +18,54 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
-
 const formSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(1,{message: "Password is required"}),
+  password: z.string().min(1, { message: "Password is required" }),
 });
 
-
 const SignInView = () => {
-
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues:{
-      email:"",
-      password:"",
-    }
+    defaultValues: {
+      email: "",
+      password: "",
+    },
   });
 
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form>Achu</form>
+          <Form {...form}>
+            <form className="p-6 md:p-8">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Welcome back Achu</h1>
+                  <p className="text-muted-foreground text-balance ">
+                    Login to your account
+                  </p>
+                </div>
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({field}) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                          type="email"
+                          placeholder="m@example.com"
+                          {...field}/>
+                        </FormControl>
+                        <FormMessage/>
+                      </FormItem>
+                    )}
+                  ></FormField>
+                </div>
+              </div>
+            </form>
+          </Form>
           <div className="hidden md:flex flex-col gapy-y-4 items-center justify-center bg-radial from-green-600 to-green-900">
             <img src="/logo.svg" className="h-[91px] w-[91px]" alt="Logo" />
             <p className="text-2xl font-semibold text-white">Meet.AI</p>
