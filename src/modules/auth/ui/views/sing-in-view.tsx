70290533d3163e8c4f054c81eb8ce0bacb39b9da
suppamann <er.yellowflash@gmail.com>
2025-07-05 09:59:02 +0530

    form added

--- src/modules/auth/ui/views/sing-in-view.tsx
+++ src/modules/auth/ui/views/sing-in-view.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { OctagonAlertIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
+import Link from "next/link";
 
+
+import { authClient } from "@/lib/auth-client";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -31,13 +36,35 @@ const SignInView = () => {
       password: "",
     },
   });
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmit =  (data: z.infer<typeof formSchema>)=>{
+    setError(null);
+
+    const {error}=  authClient.signIn.email({
+      email: data.email,
+      password: data.password,
+    },
+    {
+      onSuccess: () =>{
+        router.push("/")
+      },
+      onError: ({error})=>{
+        setError(error.message)     
+      }
+    },
+  )
+
+console.log("error is::::::::::::::::",error)
+  }
 
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
           <Form {...form}>
-            <form className="p-6 md:p-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center text-center">
                   <h1 className="text-2xl font-bold">Welcome back Achu</h1>
@@ -49,19 +76,70 @@ const SignInView = () => {
                   <FormField
                     control={form.control}
                     name="email"
-                    render={({field}) => (
+                    render={({ field }) => (
                       <FormItem>
                         <FormLabel>Email</FormLabel>
                         <FormControl>
                           <Input
-                          type="email"
-                          placeholder="m@example.com"
-                          {...field}/>
+                            type="email"
+                            placeholder="m@example.com"
+                            {...field}
+                          />
                         </FormControl>
-                        <FormMessage/>
+                        <FormMessage />
                       </FormItem>
                     )}
                   ></FormField>
+                </div>
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="*******"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+                </div>
+                {!!error && (
+                  <Alert className="bg-destructive/10 border-none">
+                    <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                    <AlertTitle>Error</AlertTitle>
+                  </Alert>
+                )}
+                <Button type="submit" className="w-full">
+                  Sign in
+                </Button>
+                <div className="relative text-center text-sm after:border-border after:absolute after:border-t after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center">
+                  <span className="text-muted-foreground bg-card relative z-10 px-2">
+                    Or continue with
+                  </span>
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <Button variant="outline" type="button" className="w-full">
+                    Google
+                  </Button>
+                  <Button variant="outline" type="button" className="w-full">
+                    Github
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have account?{" "}
+                  <Link
+                    href="/sign-up"
+                    className="underline underline-offset-4"
+                  >
+                    Sign up
+                  </Link>
                 </div>
               </div>
             </form>
@@ -72,6 +150,10 @@ const SignInView = () => {
           </div>
         </CardContent>
       </Card>
+      <div className="text-muted-foreground text-center text-xs text-balance *:[a]:hover:text-primary *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to out <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>
+      </div>
     </div>
   );
 };
