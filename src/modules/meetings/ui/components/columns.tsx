da16d5079cff246f1ced8cc5d3c31db676d10fc5
suppamann <er.yellowflash@gmail.com>
2025-08-15 22:15:44 +0530

    completed state

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -10,19 +10,10 @@ import {
   CornerDownRightIcon,
   LoaderIcon,
 } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { cn, formatDuration } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
 import { format } from "date-fns";
-import humanizeDuration from "humanize-duration";
 import { GeneratedAvatar } from "@/components/generated-avatar";
-
-function formatDuration(seconds: number) {
-  return humanizeDuration(seconds * 1000, {
-    largest: 1,
-    round: true,
-    units: ["h", "m", "s"],
-  });
-}
 
 const statusIconMap = {
   upcoming: ClockArrowUpIcon,
