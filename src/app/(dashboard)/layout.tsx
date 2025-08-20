270896075a33ddbf91abf56ae4373334ed16c604
suppamann <er.yellowflash@gmail.com>
2025-08-20 12:51:39 +0530

    Bug fixes✅

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: Props) => {
   return (
     <SidebarProvider>
       <DashboardSidebar/>
-        <main className="flex flex-col h-svh w-screen bg-gray-700">
+        <main className="flex flex-col h-svh w-screen">
         <DashboardNavbar/>
           {children}
         </main>
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
+
 export const HomeView = () => {
+  const user = authClient.useSession();
+  console.log({ user });
   return (
-    <div className=" flex flex-col min-h-svh">
-      <p className=" text-2xl text-center">ACHU🌏❤️</p>
+    <div className=" flex flex-col items-center justify-center h-screen">
+      <p className="text-2xl">
+        Welcome{" "}
+        <span className="text-[#c1c41c] font-semibold capitalize text-4xl">
+          {user.data?.user.name}!!
+        </span>
+      </p>
     </div>
   );
 };
